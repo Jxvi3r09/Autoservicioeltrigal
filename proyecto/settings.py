fdf4503ca61b59747e2dedebba3800e1d46c7896
Yeisson Gonzalez <yeissongonzalez2808@gmail.com>
2025-05-28 17:11:49 -0500

    Modificacion de estilos

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'proyecto',
     'USER': 'root',
-    'PASSWORD':'Javier',
+    'PASSWORD':'',
     'HOST': 'localhost',  # Asegúrate de que solo sea la IP
     'PORT': '3306',  # Solo el número del puerto, sin IP
 }
